b39ae40d37b1129e77c1dac68e8c60361195ee5c
akashsharmaclicks-learningaccount <131461524+akashsharmaclicks-learningaccount@users.noreply.github.com>
2023-04-28 21:41:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,6 +53,13 @@ streamlit.dataframe(my_data_row)
 
 
 
+fruit_choice2 = streamlit.text_input('What fruit would you like to add?','Kiwi')
+streamlit.write('Thank you for adding ', fruit_choice2)
+
+
+
+
+
 
 
 
