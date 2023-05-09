b0adccbcc55f4a25a23dcfbe2aed47b41835dd1d
akashsharmaclicks-learningaccount <131461524+akashsharmaclicks-learningaccount@users.noreply.github.com>
2023-05-09 22:08:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,8 +33,26 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
+
     
 streamlit.stop()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 import requests
